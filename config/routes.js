d073955de6d41986ff6ec5b45d12b8358598c0a5
shipra <shipra@canbrand.in>
2016-01-19 19:01:00 +0530

    update api chnages

--- config/routes.js
+++ config/routes.js
@@ -56,6 +56,7 @@ module.exports.routes = {
         controller: 'Message',
         action: 'conversationList'
     }
+    
 
   /***************************************************************************
   *                                                                          *
